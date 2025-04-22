a8ba065882a041c276b84a513cb30697e3fdf81f
OscarChicllaAbarca <oscarchicabar@gmail.com>
2025-04-22 17:34:09 -0500

    asdasdasd

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/Consumiapi/' 
+    ? '/Consumiapi-rama-deploy/' 
     : '/',
   chainWebpack: (config) => {
     // Agregar un rule para manejar los archivos .glb
